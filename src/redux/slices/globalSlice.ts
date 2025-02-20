3850cb13042cd0b0aa879dba75edb2463ce744f9
Master-Daniel <engrdanywiss@gmail.com>
2025-02-20 04:10:46 +0100

    added state for stats and login status

--- src/redux/slices/globalSlice.ts
+++ src/redux/slices/globalSlice.ts
@@ -5,12 +5,29 @@ interface ResetPasswordData {
     reset_token: string;
 }
 
+interface distribution {
+    month: string;
+    average_percentage: number;
+    total_evaluation: number;
+}
+
+interface Stats {
+    total_evaluation: number;
+    new_evaluations: number;
+    average_score: number;
+    evaluated_centers: number;
+    monthly_evaluation_distribution: distribution[]
+}
+
 interface GlobalState {
+    isLoggedIn: boolean;
     isSideBarOpen: boolean;
+    stats?: Stats;
     resetPasswordData: ResetPasswordData;
 }
 
 const initialState: GlobalState = {
+    isLoggedIn: false,
     isSideBarOpen: false,
     resetPasswordData: {
         email: '',
@@ -25,11 +42,17 @@ const globalSlice = createSlice({
         setResetPasswordData: (state, action: PayloadAction<Partial<ResetPasswordData>>) => {
             state.resetPasswordData = {...state.resetPasswordData, ...action.payload}
         },
+        setStats: (state, action: PayloadAction<Stats>) => {
+            state.stats = action.payload
+        },
+        setIsLoggedIn: (state, action: PayloadAction<boolean>) => {
+            state.isLoggedIn = action.payload
+        },
         setSideBarOpen: (state, action: PayloadAction<boolean>) => {
             state.isSideBarOpen = action.payload
         }
     }
 });
 
-export const { setResetPasswordData, setSideBarOpen } = globalSlice.actions;
+export const { setResetPasswordData, setSideBarOpen, setStats, setIsLoggedIn } = globalSlice.actions;
 export default globalSlice.reducer;
