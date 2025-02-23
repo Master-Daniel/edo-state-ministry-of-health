8f59857cac4f3cc4a76c1f77badb44f1002b4cf9
Master-Daniel <engrdanywiss@gmail.com>
2025-02-23 03:42:35 +0100

    updated footer logo

--- src/pages/auth/ResetPassword.tsx
+++ src/pages/auth/ResetPassword.tsx
@@ -151,7 +151,7 @@ const ResetPassword: React.FC = () => {
                     <button
                         type="submit"
                         disabled={!(formik.isValid && formik.dirty) || loading}
-                        className={`w-full rounded-md ${!(formik.isValid && formik.dirty) || loading
+                        className={`w-full cursor-pointer rounded-md ${!(formik.isValid && formik.dirty) || loading
                             ? `bg-[#AFAFAF] border border-bg-[#AFAFAF]`
                             : `bg-green-700 hover:bg-green-800`
                             } px-4 py-2 text-white transition`}
@@ -161,8 +161,8 @@ const ResetPassword: React.FC = () => {
                 </form>
 
                 {/* Footer */}
-                <div className="mt-6 text-center text-sm text-gray-500">
-                    Powered by <span className="font-semibold text-purple-600">Radah Technologies</span>
+                <div className="mt-6 justify-center gap-2 text-center flex text-sm text-gray-500 tracking-widest">
+                    Powered by <img src="/assets/images/radah-logo.png" className="font-semibold w-20 h-6" alt="" />
                 </div>
                 <div className="mt-6 text-center text-sm text-gray-500">
                     Back to <Link to="/" className="font-semibold text-green-600">Login</Link>
