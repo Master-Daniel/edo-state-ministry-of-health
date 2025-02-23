943265a0eb203d03781d6537115fcb948464eec9
Master-Daniel <engrdanywiss@gmail.com>
2025-02-23 02:44:58 +0100

    denied access if not logged in

--- src/pages/dashboard/Layout.tsx
+++ src/pages/dashboard/Layout.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import { Outlet } from "react-router-dom";
-import SideBar from '../../components/SideBar';
+import React from "react";
+import SideBar from "../../components/SideBar";
+
+import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
 
 const Layout: React.FC = () => {
+    const isLoggedIn = useSelector((state: RootState) => state.global.isLoggedIn);
+
+    if (!isLoggedIn) {
+        return <Navigate to="/" replace />;
+    }
+
     return (
         <div className="flex h-screen bg-gray-100">
             <SideBar />
             <Outlet />
         </div>
-    )
-}
+    );
+};
 
-export default Layout
+export default Layout;
